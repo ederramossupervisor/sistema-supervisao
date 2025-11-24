c119e4f2cb8ee5755a0001b6daaa20abca6e4f46
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2025-11-24 15:09:05 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,23 @@
-// firebase-config.js
+// firebase-config.js - CONFIGURAÇÃO CORRIGIDA
 const firebaseConfig = {
     apiKey: "AIzaSyCv55TRkGPiCMoQ53rmksfjb9As2rujVcE",
     authDomain: "supervisaosreac.firebaseapp.com",
     projectId: "supervisaosreac",
     storageBucket: "supervisaosreac.firebasestorage.app",
-    messagingSenderId: "693190287842",,
+    messagingSenderId: "693190287842",
     appId: "1:693190287842:web:b7d3972bc5af328d7419bb"
 };
 
 // Inicializar Firebase
 const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
+const firebaseAuth = firebase.auth();
+const firebaseDb = firebase.firestore();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-// Configurar escopos adicionais se necessário
-provider.addScope('https://www.googleapis.com/auth/drive.file');
+// Configurar domínio para login educacional
+googleProvider.setCustomParameters({
+    prompt: 'select_account',
+    hd: 'educador.edu.es.gov.br' // Restringe ao domínio educacional
+});
+
+console.log('🔥 Firebase configurado com sucesso!');
